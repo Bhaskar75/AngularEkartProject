fc759cd266b7ed60df06d104184b02df308a486d
Bhaskar Manna <34345805+Bhaskar75@users.noreply.github.com>
2025-12-25 17:05:55 +0530

    Completed video 49

--- project-name/src/app/productlist/product-list-new/product/product.component.ts
+++ project-name/src/app/productlist/product-list-new/product/product.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { NgIf } from '@angular/common';
 import { Product } from '../../../Models/Product';
+import { HighlightDirective } from '../../../CustomDirectives/highlight.directive';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [NgStyle, NgIf],
+  imports: [NgStyle, NgIf, HighlightDirective],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
